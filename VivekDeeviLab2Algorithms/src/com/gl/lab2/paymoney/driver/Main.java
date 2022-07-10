7f837c5352a9d64bd6e07c4e013129d54ef86b58
vivekvdeevi <42630775+vivekvdeevi@users.noreply.github.com>
2022-07-11 00:24:49 +0530

    Add files via upload

---
+++ VivekDeeviLab2Algorithms/src/com/gl/lab2/paymoney/driver/Main.java
@@ -0,0 +1,12 @@
+package com.gl.lab2.paymoney.driver;
+
+import com.gl.lab2.paymoney.services.LinearSearch;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		LinearSearch linearsearch = new LinearSearch();
+		linearsearch.implementationOfLinearSearch();
+	}
+
+}
